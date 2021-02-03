e0e92da76f251eef811351804f203a27b3a0263d
kindacommander <artem.sandyuk@gmail.com>
2021-02-04 00:17:17 +0200

    Fetching current lamp state

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const addr = "192.168.31.68:8888"
@@ -21,6 +23,15 @@ type Command struct {
 
 func (c *Command) String() string {
 	return c.Feature + fmt.Sprint(c.Value)
+}
+
+// Values contain current lamp config
+type Values struct {
+	PoweredOn  bool `json:"power"`
+	Effect     int  `json:"effect"`
+	Brightness int  `json:"brightness"`
+	Speed      int  `json:"speed"`
+	Scale      int  `json:"scale"`
 }
 
 func main() {
@@ -56,6 +67,51 @@ func setupRoutes() {
 
 		commands <- cmd
 	})
+	http.HandleFunc("/curr-features", func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		conn, err := net.Dial("udp", addr)
+		if err != nil {
+			panic(err)
+		}
+
+		cmd := Command{"GET", 0}
+		conn.Write([]byte(cmd.String()))
+
+		res := make([]byte, 16)
+		conn.Read(res)
+		conn.Close()
+
+		fmt.Println(res)
+		valuesStr := strings.Split(strings.Trim(string(res), "CURR "), " ")
+		fmt.Println(valuesStr)
+
+		vals := make([]int, 5)
+		for i, val := range valuesStr {
+			v, err := strconv.Atoi(val)
+			if err != nil {
+				panic(err)
+			}
+			vals[i] = v
+		}
+
+		values := Values{PoweredOn: itob(vals[4]), Effect: vals[0], Brightness: vals[1], Speed: vals[2], Scale: vals[3]}
+		json, err := json.Marshal(values)
+		if err != nil {
+			panic(err)
+		}
+		w.Write(json)
+	})
+}
+
+func itob(i int) bool {
+	if i == 0 {
+		return false
+	}
+	return true
 }
 
 func enableCors(w *http.ResponseWriter) {
